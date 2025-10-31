d77fdc0890f3e90bdad96e16594c4609352e8513
Ryner Lute <rynlute@gmail.com>
2025-10-31 11:20:35 +0300

    fix again

---
+++ __init__.py
@@ -0,0 +1,13 @@
+﻿# dbf_sync/__init__.py
+"""
+DBF Sync Refactor package initializer.
+
+"""
+
+from .dbf_sync import main
+
+__all__ = ["main"]
+
+__version__ = "2.0.0"
+__author__ = "Ryner Lute"
+__license__ = "MIT"
